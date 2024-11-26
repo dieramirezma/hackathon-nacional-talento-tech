0d1941dac73b3633c2f62a1e34cc41cc4e74524f
dieramirezma <dieramirezma@unal.edu.co>
2024-11-25 23:46:49 -0500

    chore: config tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        agroGreen: {
+          dark: "#6B8E23",
+          ligth: "#32CD32",
+          medium: "#66C266",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
 
