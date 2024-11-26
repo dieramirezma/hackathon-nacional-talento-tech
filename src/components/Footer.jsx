a099931f48f64296cdbdace14b43e16700ce10c8
dieramirezma <dieramirezma@unal.edu.co>
2024-11-26 00:02:17 -0500

    feat: add Footer component with copyright information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; 2021 <a href="https://github.com/dieramirezma/" target="_blank" rel="noopener noreferrer">dieramirezma</a>
+      </p>
+    </footer>
+  )
+}
